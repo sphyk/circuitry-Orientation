7ff0d734212a6cd7607865a45e1a2f4b7877ca25
Jacob Turola <60889730+sphyk@users.noreply.github.com>
2025-02-18 18:03:07 -0500

    Changed blink to be saved properly

--- Blink_example/Blink_example.ino
+++ Blink_example/Blink_example.ino
@@ -1,38 +1,31 @@
 
 /*
- _____           _       _ 
-/  ___|         (_)     | |
-\ `--.  ___ _ __ _  __ _| |
- `--. \/ _ \ '__| |/ _` | |
-/\__/ /  __/ |  | | (_| | |
-\____/ \___|_|  |_|\__,_|_|
-                           
-Not cereal
 
-      o8Oo./
-   ._o8o8o8Oo_.
-    \========/
-     `------'  ⠀⠀⠀⠀⠀⠀⠀
+______ _     _____ _   _  _   __
+| ___ \ |   |_   _| \ | || | / /
+| |_/ / |     | | |  \| || |/ / 
+| ___ \ |     | | | . ` ||    \ 
+| |_/ / |_____| |_| |\  || |\  \
+\____/\_____/\___/\_| \_/\_| \_/
+                                
+                                ⠀⠀⠀⠀⠀⠀⠀
 */                              
 //Jacob Turola for Markham Campus Library Makerspace                               
 //This code shows how to setup the serial monitor
 
 
-int input = 10;//creates an integer variable that sets pin 10 to be named "inputPin" this can be attached to a sensor or button
-int receiver = 11; //creates an integer variable that sets pin 11 to be named "receiverPin" <- Case Sensitive!
+int lightpin = 10;//creates an integer variable that sets pin 10 to be named "lightpin"
 
 void setup() { //this code runs once! also make sure to fit all of your setup code in between these curly brackets
 
-  pinMode(input,OUTPUT);// sets our output pin to output electricity
-  pinMode(receiver,INPUT);//sets the receiver to become an input
-  Serial.begin(9600); //initialises the serial monitor with a baud rate of 9600
+  pinMode(lightpin,OUTPUT);// sets our output pin to output electricity
 }
 
 void loop() { //this will loop infinitely
 
-  int data = digitalRead(receiver);//creates a variable that is the value of whatever enters pin 11
-  Serial.println(data); //prints the data in the serial monitor and makes a new line
-  delay(10); //short delay so its easier to handle
-  
-
+  digitalWrite(lightpin,HIGH);//turns on LED
+  delay(1000); // wait 1 sec
+  digitalWrite(lightpin,LOW); //turns off led
+  delay(1000);//wait 1 sec
+  //now it will loop again
 }
